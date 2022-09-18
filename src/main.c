69cda1060e9f0116fbd19577cc4e02bc2817df06
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-18 03:39:42 +0100

    meh

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 01:56:59 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/18 03:00:23 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/18 03:35:57 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int c, char **v)
 {
-	if (c != 2)
+	if (c != 2 || !v[1])
 	{
 		printf("\033[0;31mA map in format `*.cub` is required!\033[0;37m\n");
 		return (EXIT_FAILURE);
